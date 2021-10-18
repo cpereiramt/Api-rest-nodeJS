7b3be0cef06d1e8721faca55be1127a5c5e09e56
cpereiramt <cpereiramt@gmail.com>
2021-10-18 13:49:07 -0400

    salvando alterações na variavel de url da conexão do banco

--- database/configuration.js
+++ database/configuration.js
@@ -1,8 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
-MongoClient.connect(
-  `mongodb://${process.env.MONGODBPATH}:${process.env.MONGODBPORT}`
-).then((conn) => {
+MongoClient.connect(process.env.DATABASE_URL).then((conn) => {
   console.log("Connected to MongoDB");
   global.conn = conn.db(process.env.MONGODATABASE);
 });
